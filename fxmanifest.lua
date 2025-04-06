1a143a21e2e44e1262cffbc7c484b5f984acef57
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-04-05 23:16:36 -0400

    add ytyp for custom props

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,14 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
-author 'Xinerki'
-name 'kgv-drone'
-description 'teehee drone'
-lua54 'ya'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+
+author 'Xinerki + Popcorn RP'
+description 'A realistic and explosive drone resource'
+name 'prp-drone'
+
 client_script 'client.lua'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/rpemotesreborn_props.ytyp'
